466f9eb15dfad0869dd1ef2c2668d5d7ded50561
Soniya Gosavi <59673038+soniyagosavi10@users.noreply.github.com>
2021-09-23 15:12:33 -0700

    Update score.py

--- inference/score.py
+++ inference/score.py
@@ -17,8 +17,8 @@ from sklearn.tree import DecisionTreeClassifier
 Inference script for IRIS Classification:
 
 '''
-__author__ = "Srijith.S"
-__email__ = "srssreejith@gmail.com"
+__author__ = "soniyagosavi10"
+__email__ = "soniyagosavi10@gmail.com"
 
 def init():
     '''
