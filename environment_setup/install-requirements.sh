6d5672c36ea25d01d765a9f3c927b281e7d14fab
Soniya Gosavi <59673038+soniyagosavi10@users.noreply.github.com>
2021-09-24 11:31:06 -0700

    Update install-requirements.sh

--- environment_setup/install-requirements.sh
+++ environment_setup/install-requirements.sh
@@ -1,7 +1,8 @@
 python --version
-#pip install --upgrade azure-cli
-pip install azure-cli==2.20.0
-pip install --upgrade azureml-sdk[cli] -use-deprecated=legacy-resolver  
+pip install --upgrade azure-cli
+#pip install azure-cli==2.20.0
+pip install --upgrade azureml-sdk[cli] 
+#-use-deprecated=legacy-resolver  
 pip install pytest
 pip install pytest-cov
 #pip install -r requirements.txt
